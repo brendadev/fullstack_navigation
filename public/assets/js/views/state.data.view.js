be670fba166f3075d9d466bf5ab74873841a1dd8
thehme <bcortez23@gmail.com>
2015-11-30 10:17:51 -0500

    Revert "moved var declaration out of switch statement"
    This reverts commit 51760d48d3f70d88b59470008c6f6840362d6ad2.

--- public/assets/js/views/state.data.view.js
+++ public/assets/js/views/state.data.view.js
@@ -27,14 +27,13 @@ define([
             //displayName: "uninsured"
             //displayName: "population"
             //debugger;
-            var d = document.createElement('div');
             _.each(this.columns,function(c){
                 console.log(c.displayName);
                 switch(c.displayName) {
                     case 'state name':
                         //RE: the next 2 lines are repeated so can go above _.each(this.columns...
                         //RE: first to admit I don't always do this myself and should do
-                        //var d = document.createElement('div');
+                        var d = document.createElement('div');
                         d.className = 'span' + this.columns.length + 'col rowCell left';
                         d.innerHTML = this.model.get('name');
                         //RE: I would put this next line outside the switch
@@ -44,7 +43,7 @@ define([
                         //debugger;
                         break;
                     case 'insured':
-                        //var d = document.createElement('div');
+                        var d = document.createElement('div');
                         d.className = 'span' + this.columns.length + 'col rowCell left';
                         d.innerHTML = this.model.get('number_insured');
                         frag.appendChild(d);
@@ -52,7 +51,7 @@ define([
                         //debugger;
                         break;
                     case 'uninsured':
-                        //var d = document.createElement('div');
+                        var d = document.createElement('div');
                         d.className = 'span' + this.columns.length + 'col rowCell left';
                         d.innerHTML = this.model.get('number_uninsured');
                         frag.appendChild(d);
@@ -60,7 +59,7 @@ define([
                         //debugger;
                         break;
                     case 'population':
-                        //var d = document.createElement('div');
+                        var d = document.createElement('div');
                         d.className = 'span' + this.columns.length + 'col rowCell left';
                         d.innerHTML = this.model.get('population');
                         frag.appendChild(d);
